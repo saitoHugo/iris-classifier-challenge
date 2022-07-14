d881a640af236020e567a3cd4bce206975de968d
saitoHugo <hugovcsaito@gmail.com>
2022-07-14 17:24:38 -0300

    add psedocode for train and predict

--- iris-api/main.py
+++ iris-api/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from pathlib import Path
 
 app = FastAPI()
 
+
+"""
+TODO: 
+    - load all models gloabally
+    - create a train route
+    - create a predict route based on model selection
+    - extra route get_all_trained_models
+    - download local data or download from bucket
+"""
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
 @app.get("/", include_in_schema=False)
 async def docs_redirect():
     return RedirectResponse(url='/docs')
+
