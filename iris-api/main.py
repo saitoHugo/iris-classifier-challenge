8458f36784cbe08d0fcb661d2ceb6debfb12cc95
saitoHugo <hugovcsaito@gmail.com>
2022-07-15 08:40:36 -0300

    add base train and predict route and loggger

--- iris-api/main.py
+++ iris-api/main.py
@@ -4,6 +4,10 @@ from fastapi.responses import RedirectResponse
 from pathlib import Path
 from utils import utils
 from typing import List
+import logging
+from utils import global_varibles
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
 
@@ -17,8 +21,9 @@ TODO:
 
     - add logging
 """
-
-BASE_DIR = Path(__file__).resolve(strict=True).parent
+# #TODO: define global base dir
+# global BASE_DIR
+# BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 #load datset globally
 
@@ -26,13 +31,27 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent
 
 
 ### pydantic models
-class Input(BaseModel):
+class PredictionInput(BaseModel):
     model_name: str
     Inputs: List[float]
 
 
 
-class Prediction(BaseModel):
+class PredictionOutput(BaseModel):
+    model_name: str
+    label: str
+    value: int
+
+class TrainInput(BaseModel):
+    #TODO:update attributes
+    model_name: str
+    Inputs: List[float]
+
+
+
+class TrainOutput(BaseModel):
+    #TODO:update attributes
+    model_name: str
     label: str
     value: int
 
@@ -41,14 +60,37 @@ class Prediction(BaseModel):
 async def docs_redirect():
     return RedirectResponse(url='/docs')
 
-@app.post("/predict", response_model=Prediction, status_code=200)
-def get_prediction(payload: Input):
+@app.post("/predict", response_model=PredictionOutput, status_code=200)
+def get_prediction(payload: PredictionInput):
+    logger.info("/predict route init")
     model_name = payload.model_name
 
-    prediction_list = utils.predict(ticker)
+    prediction_list = utils.predict(model_name)
+    logger.info("utils.predict executed")
+    
 
     if not prediction_list:
         raise HTTPException(status_code=400, detail="Model not found.")
+    
+    value = prediction_list[0]
 
-    response_object = {"inputs": ticker, "prediction": prediction_list}
+    #convert to label
+    label = utils.convert_value_to_label(value)
+    logger.info("utils.convert_value_to_label executed")
+    
+    response_object = {"model_name": model_name, "label":label, "value": value}
     return response_object
+
+
+@app.post("/train", response_model=TrainOutput, status_code=200)
+def get_prediction(payload: TrainInput):
+    logger.info("/train route init")
+    model_name = payload.model_name
+
+    prediction_list = utils.train(model_name)
+    logger.info("utils.predict executed")
+    
+
+    if not prediction_list:
+        raise HTTPException(status_code=400, detail="Model not found.")
+    
