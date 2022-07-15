5c38f709daab39b464ac426af82534978b146e57
saitoHugo <hugovcsaito@gmail.com>
2022-07-15 08:41:49 -0300

    add inital structure to train and predict aux functions

--- iris-api/utils/utils.py
+++ iris-api/utils/utils.py
@@ -1,4 +1,6 @@
 from typing import List
+from pathlib import Path
+import global_varibles
 
 def train():
     #download dataset
@@ -16,7 +18,7 @@ def train():
     pass
 
 def predict(model_name:str, input:List[float]) -> str:
-    model_path = Path(BASE_DIR).joinpath(f"{ticker}.joblib")
+    model_path = Path(global_varibles.BASE_DIR).joinpath(f"{model_name}.pkl")
     if not model_path.exists():
         return Exception(f"Could not find model at {model_path}")
     pass
@@ -27,3 +29,14 @@ def predict(model_name:str, input:List[float]) -> str:
     #execute prediction
 
     #return output class by name (str)
+
+def convert_value_to_label(prediction_value:int):
+    label_decode_dict = {
+        0 : 'flower0',
+        1 : 'flower1',
+        2 : 'flower2',
+    }
+
+    if prediction_value not in label_decode_dict.keys():
+        raise Exception(f"predict value given {prediction_value} does not exists in decode dict. Options are 0, 1 or 2 (int).")
+    return label_decode_dict[prediction_value]
